b932d9d715b340baf8d9f804a96322f87ab37a32
gureuso <wyun13043@gmail.com>
2022-11-14 19:52:30 +0900

    Clean code

--- app/common/cron.py
+++ app/common/cron.py
@@ -3,6 +3,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 scheduler = AsyncIOScheduler(timezone='Asia/Seoul')
 
 
-@scheduler.scheduled_job('cron', second='*/10', id='cron_test_01')
+@scheduler.scheduled_job('cron', second='10', id='cron_test_01')
 async def cron_test_01():
     print(1)
