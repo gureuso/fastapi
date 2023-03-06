02d75a8b1e1843c1a6c53f9d43b063ac7db63260
구르소 <wyun13043@gmail.com>
2023-03-06 13:55:40 +0900

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
         'app.main:app',
         host=Config.APP_HOST,
         port=Config.APP_PORT,
-        debug=not Config.is_production(),
         reload=not Config.is_production(),
         workers=4,
         log_level='info' if not Config.is_production() else 'warning',
